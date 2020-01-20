ed9181873fcc23a1199a8074cf4449fddd72d8dc
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-01-20 18:10:02 +0100

    Fixed product detail page

--- routes/product.js
+++ routes/product.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose'); 
 
+var Product = require('../models/product');
+
 mongoose.connect('mongodb://localhost:27017/ecommercestore'); 
 var db=mongoose.connection; 
 db.on('error', console.log.bind(console, "connection error")); 
@@ -11,14 +13,15 @@ db.once('open', function(callback){
 
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  db.collection('products').find({_id: req.body._id}).toArray( function(err, data){
+router.get('/:id', (req, res, next) => {
+    
+  Product.findOne({_id: req.params.id}, function(err, docs){
     if(err) {
               res.json(err);
    }
     else {
-      res.render('product', {title: 'E-Commerce || Product', email: req.cookies.email, product: data});
-            console.log(data);
+            res.render('product', {title: 'E-Commerce || Product', product: docs, email: req.cookies.email });
+            console.log(docs);
     }
   });
 
