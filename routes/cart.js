4c65cfc02f71e9aee745dc32a8c8ca57c218ad36
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-01-27 19:19:55 +0100

    Fixed cart prices and cart items

--- routes/cart.js
+++ routes/cart.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session); 
+var Handlebars = require('hbs');
 
 var Product = require('../models/product');
 
@@ -35,10 +36,11 @@ router.get('/', (req, res, next) => {
         displayCart.total = total;
 
         //Render Cart
-      res.render('cart', { title: 'E-Commerce || Cart', email: req.cookies.email, cart: cart});
+      res.render('cart', { title: 'E-Commerce || Cart', email: req.cookies.email, cart: cart});//cart:displayCart
 });
 
 router.post('/:id', function (req, res) {
+  var quantity = req.body.quantity;
   req.session.cart = req.session.cart || {};
   var cart = req.session.cart;
   console.log(cart);
@@ -57,7 +59,7 @@ Product.findOne({_id:req.params.id}, function(err,product){
               item:product._id,
               title: product.title,
               price: product.price,
-              qty: 1,
+              qty: quantity,
               imagePath: product.imagePath
           }
           console.log(cart);
@@ -67,5 +69,8 @@ Product.findOne({_id:req.params.id}, function(err,product){
   });
 });
 
+Handlebars.registerHelper('totalP', function(price, qty){
+  return price * qty;
+})
 
 module.exports = router;
