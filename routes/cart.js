aa42bdd27ea1c2c85b3365d60f89bc37e6a4b3bd
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-03-08 21:42:11 +0100

    working on adding deals to cart

--- routes/cart.js
+++ routes/cart.js
@@ -6,6 +6,7 @@ var MongoStore = require('connect-mongo')(session);
 var Handlebars = require('hbs');
 
 var Product = require('../models/product');
+var Deal = require('../models/deals');
 
 mongoose.connect('mongodb://localhost:27017/ecommercestore'); 
 var db=mongoose.connection; 
@@ -56,7 +57,40 @@ router.post('/:id', function (req, res) {
 Product.findOne({_id:test}, function(err,product){
    console.log(product);
       if(err){
+          //console.log(err);
+          Product.findOne({_id:test}, function(err,product){
+   console.log(product);
+      if(err){
           console.log(err);
+      }
+      if(cart[req.params.id]){
+          cart[req.params.id].qty++;
+
+      }
+      else{
+          cart[req.params.id] = {
+              item:product._id,
+              title: product.title,
+              price: product.price,
+              qty: quantity,
+              imagePath: product.imagePath
+          }
+          console.log(cart);
+      }
+
+      var totalQuantity = 0;
+      //Get Total
+      for(var item in cart){
+        totalQuantity += Number(cart[item].qty);
+    }
+    console.log(totalQuantity);
+    res.cookie('quant', totalQuantity, {maxAge: 180*60*1000});
+      //res.render('./cart', {cart: cart, message:'Added to cart successfully', success:'message'});
+      //res.clearCookie('quant');
+      
+      
+    res.redirect('/cart');
+  });
       }
       if(cart[req.params.id]){
           cart[req.params.id].qty++;
