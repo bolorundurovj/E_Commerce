c80c47f7692401a5e2430dff442b944551c38d1f
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-04-12 17:18:11 +0100

    added more tests

--- test/test-pages.js
+++ test/test-pages.js
@@ -5,21 +5,74 @@ const app = require('../app');
 const server = supertest(app);
 
 describe("Url Check", () => {
-    it('Main page status', function(done) {
+    it('HomePage Status', function(done) {
         this.timeout(15000);
         server
         .get('/')
         .end((err, res) => {
-            expect(200);
+            expect(res.status).to.equal(200);
             if (err) done(err);
             done();
         });
     });
-    
-    // it('Profile page content', function(done) {
-    //     request('http://localhost:3000/profile' , function(error, response, body) {
-    //         expect(response.statusCode).to.equal(200);
-    //         done();
-    //     });
-    // });
+    it('LoginPage Status', function(done) {
+        this.timeout(15000);
+        server
+        .get('/login')
+        .end((err, res) => {
+            expect(res.status).to.equal(200);
+            if (err) done(err);
+            done();
+        });
+    });
+    it('RegisterPage Status', function(done) {
+        this.timeout(15000);
+        server
+        .get('/register')
+        .end((err, res) => {
+            expect(res.status).to.equal(200);
+            if (err) done(err);
+            done();
+        });
+    });
+    it('Direct ProductPage Status', function(done) {
+        this.timeout(15000);
+        server
+        .get('/product')
+        .end((err, res) => {
+            expect(res.status).to.equal(404);
+            if (err) done(err);
+            done();
+        });
+    });
+    it('Direct DealPage Status', function(done) {
+        this.timeout(15000);
+        server
+        .get('/deal')
+        .end((err, res) => {
+            expect(res.status).to.equal(404);
+            if (err) done(err);
+            done();
+        });
+    });
+    it('Real Specific DealPage Status', function(done) {
+        this.timeout(15000);
+        server
+        .get('/deal/5e2736ac4380f63124bcae33')
+        .end((err, res) => {
+            expect(res.status).to.equal(200);
+            if (err) done(err);
+            done();
+        });
+    });
+    it('ProfilePage Protect', function(done) {
+        this.timeout(15000);
+        server
+        .get('/profile')
+        .end((err, res) => {
+            expect(res.status).to.equal(200);
+            if (err) done(err);
+            done();
+        });
+    });
 });
