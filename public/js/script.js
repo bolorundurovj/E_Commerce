f8becba53211833c5d0a14c9148184a1305a1791
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-05 17:31:36 +0100

    formmated script

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,18 @@
 // some scripts
 
 // jquery ready start
-$(document).ready(function() {
-	// jQuery code
+$(document).ready(function () {
+  // jQuery code
 
+  //////////////////////// Prevent closing from click inside dropdown
+  $(document).on("click", ".dropdown-menu", function (e) {
+    e.stopPropagation();
+  });
 
-    
-
-	//////////////////////// Prevent closing from click inside dropdown
-    $(document).on('click', '.dropdown-menu', function (e) {
-      e.stopPropagation();
-    });
-
-
-    
-
-	//////////////////////// Bootstrap tooltip
-	if($('[data-toggle="tooltip"]').length>0) {  // check if element exists
-		$('[data-toggle="tooltip"]').tooltip()
-	} // end if
-
-
-
-
-    
-}); 
+  //////////////////////// Bootstrap tooltip
+  if ($('[data-toggle="tooltip"]').length > 0) {
+    // check if element exists
+    $('[data-toggle="tooltip"]').tooltip();
+  } // end if
+});
 // jquery end
-
