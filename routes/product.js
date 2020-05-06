98187304fc773a3acba74e31c453bc786d7ceb88
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-06 09:12:16 +0100

    code refactoring

--- routes/product.js
+++ routes/product.js
@@ -1,36 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mongoose = require('mongoose'); 
+var mongoose = require("mongoose");
 
-var Product = require('../models/product');
-var Cart = require('../models/cart');
+var Product = require("../models/product");
+var Cart = require("../models/cart");
 
 //mongodb://admin:admin1234@ds014808.mlab.com:14808/ecommerceapp
 //mongodb://localhost:27017/ecommercestore
 
-mongoose.connect('mongodb://localhost:27017/ecommercestore'); 
-var db=mongoose.connection; 
-db.on('error', console.log.bind(console, "connection error")); 
-db.once('open', function(callback){ 
-    console.log("Database connection succeeded product"); 
-})
-
+mongoose.connect("mongodb://localhost:27017/ecommercestore");
+var db = mongoose.connection;
+db.on("error", console.log.bind(console, "connection error"));
+db.once("open", function (callback) {
+  console.log("Database connection succeeded product");
+});
 
 /* GET home page. */
-router.get('/:id', (req, res, next) => {
-    
-  Product.findOne({_id: req.params.id}, function(err, docs){
-    if(err) {
-             console.log(err);
-   }
-    else {
-            res.render('product', {title: 'E-Commerce || Product', product: docs, email: req.cookies.email, namee: req.cookies.cc, quant: req.cookies.quant});
-            console.log(docs);
+router.get("/:id", (req, res, next) => {
+  Product.findOne({ _id: req.params.id }, function (err, docs) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("product", {
+        title: "E-Commerce || Product",
+        product: docs,
+        email: req.cookies.email,
+        namee: req.cookies.cc,
+        quant: req.cookies.quant,
+      });
+      console.log(docs);
     }
   });
 });
-
-
-
 
 module.exports = router;
