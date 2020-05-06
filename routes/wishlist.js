5ac6e981ad4af1e6ae8d0e0d240e69681f11dfe8
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-06 09:13:10 +0100

    code refactoring

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('wishlist', { title: 'E-Commerce || My WishList', email: req.cookies.email, namee: req.cookies.cc, quant: req.cookies.quant});
+router.get("/", (req, res, next) => {
+  res.render("wishlist", {
+    title: "E-Commerce || My WishList",
+    email: req.cookies.email,
+    namee: req.cookies.cc,
+    quant: req.cookies.quant,
+  });
 });
 
 module.exports = router;
