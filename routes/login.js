b850e1295cf77c6ad85f39d09e7d384f9a7e284f
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-06 09:11:46 +0100

    code refactoring

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('login', { title: 'E-Commerce || Login', email: req.cookies.email, namee: req.cookies.cc, quant: req.cookies.quant});
-  
+router.get("/", (req, res, next) => {
+  res.render("login", {
+    title: "E-Commerce || Login",
+    email: req.cookies.email,
+    namee: req.cookies.cc,
+    quant: req.cookies.quant,
+  });
 });
 
 module.exports = router;
