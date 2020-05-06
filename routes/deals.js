7c67354636e8ea03a4ca0fe97e42a81944fba4bf
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-06 09:10:38 +0100

    code refactoring

--- routes/deals.js
+++ routes/deals.js
@@ -1,35 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mongoose = require('mongoose'); 
+var mongoose = require("mongoose");
 
-var Deal = require('../models/deals');
-var Cart = require('../models/cart');
+var Deal = require("../models/deals");
+var Cart = require("../models/cart");
 
 //mongodb://admin:admin1234@ds014808.mlab.com:14808/ecommerceapp
 //mongodb://localhost:27017/ecommercestore
 
-mongoose.connect('mongodb://localhost:27017/ecommercestore'); 
-var db=mongoose.connection; 
-db.on('error', console.log.bind(console, "connection error")); 
-db.once('open', function(callback){ 
-    console.log("Database connection succeeded deals"); 
-})
+mongoose.connect("mongodb://localhost:27017/ecommercestore");
+var db = mongoose.connection;
+db.on("error", console.log.bind(console, "connection error"));
+db.once("open", function (callback) {
+  console.log("Database connection succeeded deals");
+});
 
-
-
-router.get('/:id', (req, res, next) => {
-    
-  Deal.findOne({_id: req.params.id}, function(err, docs){
-    if(err) {
-              res.json(err);
-   }
-    else {
-            res.render('deal', {title: 'E-Commerce || Deals&Offers', deal: docs, email: req.cookies.email, namee: req.cookies.cc, quant: req.cookies.quant});
-            console.log(docs);
+router.get("/:id", (req, res, next) => {
+  Deal.findOne({ _id: req.params.id }, function (err, docs) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.render("deal", {
+        title: "E-Commerce || Deals&Offers",
+        deal: docs,
+        email: req.cookies.email,
+        namee: req.cookies.cc,
+        quant: req.cookies.quant,
+      });
+      console.log(docs);
     }
   });
 });
-
-
 
 module.exports = router;
